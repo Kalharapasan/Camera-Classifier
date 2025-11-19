385b3c9827809aff0de746748e03ef0fe3a071ba
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-11-19 20:50:43 +0530

    add constructer in camera class

--- camera.py
+++ camera.py
@@ -2,3 +2,13 @@ import cv2 as cv
 import threading
 from collections import deque
 
+class Camera:
+    def __init__(self, camera_id=0):
+        self.camera = cv.VideoCapture(camera_id)
+        if not self.camera.isOpened():
+            raise ValueError(f"Unable to open camera {camera_id}!")
+
+        self.width = int(self.camera.get(cv.CAP_PROP_FRAME_WIDTH))
+        self.height = int(self.camera.get(cv.CAP_PROP_FRAME_HEIGHT))
+        
+        
